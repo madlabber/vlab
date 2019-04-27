c7dd76fb420c7afebee5b7ded4286397a7df410d
madlabber <administrator@NUC.LAB>
2019-04-27 10:51:51 -0700

    Update show-dashboard-html.ps1

--- show-dashboard-html.ps1
+++ show-dashboard-html.ps1
@@ -20,16 +20,20 @@ $result=invoke-command -session $session -scriptblock {
     #write-host "<br>"
 
     $VMHosts=get-cluster $conf.VICluster | get-vmhost
+    $CpuUsageMhz=($VMHosts | measure-object -property CpuUsageMhz -sum).sum
+    $CpuTotalMhz=($VMHosts | measure-object -property CpuTotalMhz -sum).sum
+    $MemoryUsageGB=($VMHosts | measure-object -property MemoryUsageGB -sum).sum
+    $MemoryTotalGB=($VMHosts | measure-object -property MemoryTotalGB -sum).sum
     $NCAggrs=$(foreach ($aggr in $((get-ncvol | where  {$_.Name -like "lab_*"}).aggregate | sort-object | get-unique)){get-ncaggr $aggr})
     Write-Host "</center><table><tr><td align=center>Hosts</td><td> </td><td align=center>CPU</td><td> </td><td align=center>Memory</td><td> </td><td width=25% align=center>Storage</td></tr>"
     Write-Host "<tr>"
     Write-Host "<td align=center><h1> "$VMHosts.count" <h1></td><td width=25px> </td>"
-    Write-Host "<td align=center><h1> "$($VMHosts.CpuUsageMhz/$VMHosts.CpuTotalMhz).tostring('p0').Replace(' ','')" </h1></td><td width=25px> </td>"
-    Write-Host "<td align=center><h1> "$($VMHosts.MemoryUsageGB/$VMHosts.MemoryTotalGB).tostring('p0').Replace(' ','')" </h1></td><td width=25px> </td>"
+    Write-Host "<td align=center><h1> "$($CpuUsageMhz/$CpuTotalMhz).tostring('p0').Replace(' ','')" </h1></td><td width=25px> </td>"
+    Write-Host "<td align=center><h1> "$($MemoryUsageGB/$MemoryTotalGB).tostring('p0').Replace(' ','')" </h1></td><td width=25px> </td>"
     Write-Host "<td align=center> -- </td>" 
     Write-Host "<tr><td align=center></td><td></td>"
-    Write-Host "<td align=center><h6> "$VMHosts.CpuUsageMhz"/"$VMHosts.CpuTotalMhz"Mhz </h6></td><td> </td>"
-    Write-Host "<td align=center><h6> "$VMHosts.MemoryUsageGB.tostring("n2")"/"$VMHosts.MemoryTotalGB.tostring("n2")"GB </h6></td><td width=25px> </td>"
+    Write-Host "<td align=center><h6> "$CpuUsageMhz" / "$CpuTotalMhz" Mhz </h6></td><td> </td>"
+    Write-Host "<td align=center><h6> "$MemoryUsageGB.tostring("n2")" /"$MemoryTotalGB.tostring("n2")"GB </h6></td><td width=25px> </td>"
     Write-Host "<td align=center> -- </td>" 
     Write-Host "</tr></table></center>"
     # foreach ($aggr in $((get-ncvol | where  {$_.Name -like "lab_*"}).aggregate | sort-object | get-unique)){get-ncaggr $aggr}
