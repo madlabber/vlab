77ff643694d72e2aecec31797bb85c41a7ed4d37
madlabber <49624568+madlabber@users.noreply.github.com>
2019-05-24 20:09:47 -0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -39,24 +39,47 @@ Set-PowerCLIConfiguration -Scope AllUsers -DefaultVIServerMode Multiple -confirm
 $IISFeatures = "Web-Server","Web-WebServer","Web-Common-Http","Web-Default-Doc","Web-Dir-Browsing","Web-Http-Errors","Web-Static-Content","Web-Health","Web-Http-Logging","Web-Performance","Web-Stat-Compression","Web-Security","Web-Filtering","Web-App-Dev","Web-Net-Ext45","Web-Asp-Net45","Web-ISAPI-Ext","Web-ISAPI-Filter","Web-WebSockets","Web-Mgmt-Tools","Web-Mgmt-Console"
 Install-WindowsFeature -Name $IISFeatures
 
+# Create a directory for downloaded bits
+If(!(Test-Path "$PSScriptRoot")) { New-Item -Path "$PSScriptRoot" -Name "setup" -ItemType "directory" }
+
 # Install PSTK (Manual step)
 # Install Node.js
-# npm install nodemon -g
+$nodejs_url = "https://nodejs.org/dist/v10.15.3/node-v10.15.3-x64.msi"
+$nodejs_exe = "$PSScriptRoot\setup\node-v10.15.3-x64.msi"
+if(!(Test-Path "$nodejs_exe")){
+    write-host "Downloading node.js."
+    $wc = New-Object System.Net.WebClient
+    $wc.DownloadFile($nodejs_url, $nodejs_exe)
+    write-Output "Download complete."
+}
+write-host
+Write-host "Installing node.js"
+Start-Process $nodejs_exe -Wait
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
-#Install express:
-# cd c:\vlab
-# npm install express
+# npm install nodemon -g
+Write-host
+Write-Host "Installing nodemon."
+npm install nodemon -g
+Write-host
+Write-Host "Installing express."
+npm install express
 
 #Install myrtille, be sure to change the default port to 8083
 $myrtille_url = "https://github.com/cedrozor/myrtille/releases/download/v2.3.1/Myrtille_2.3.1_x86_x64_Setup.exe"
 $myrtille_exe = "$PSScriptRoot\setup\Myrtille_2.3.1_x86_x64_Setup.exe"
-write-host "downloading the Myrtille for windows installer"
-$result=New-Item -Path "$PSScriptRoot" -Name "setup" -ItemType "directory"
-$start_time = Get-Date
+$myrtille_msi = "$PSScriptRoot\setup\Myrtille.msi"
+write-host "Downloading Myrtille."
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($myrtille_url, $myrtille_exe)
-write-Output "Myrtille installer downloaded"
-write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+write-Output "Download complete."
+write-host
+Write-host "Extracting Myrtille"
+Start-Process "$myrtille_exe" "-y" -Wait
+write-host
+Write-host "Installing Myrtille"
+Start-Process "$myrtille_msi" -Wait
+
 
 #retreive myrtille password hash
 #  https://localhost/myrtille/GetHash.aspx?password=P@ssw0rd
